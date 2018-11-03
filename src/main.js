6b44f7354a9290244e93eb1e914a512c8c0c8689
unknown <mcintoshdaron@gmail.com>
2018-11-03 17:48:58 -0400

    hackathon

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import router from './router'
 import BootstrapVue from "bootstrap-vue"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
+import * as VueGoogleMaps from 'vue2-google-maps'
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyBUx2kl5YNFZVeNWUMReLoSmGVgK5v8gWo",
+    libraries: 'places',
+  }
+})
 Vue.use(BootstrapVue)
 
 new Vue({
